1864d7902d5b104ea0a4a3708a255d2ec82fb24b
unknown <punniyamoorthy898@gmail.com>
2022-12-13 12:23:28 +0530

    success

---
+++ app/views.py
@@ -0,0 +1,130 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib import messages
+from app.form import CustomUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import JsonResponse
+import json
+
+def home(request):
+    products = Product.objects.filter(trending=1)
+    return render(request, "shop/index.html", {"products":products})
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect("/")
+    else:
+        if request.method == 'POST':
+            name = request.POST.get('username')
+            pwd = request.POST.get('password')
+            user = authenticate(request, username=name, password=pwd)
+            if user is not None:
+                login(request,user)
+                messages.success(request, "Login Successfully")
+                return redirect('/')
+            else:
+                messages.error(request, "Invail Mail Id or Password")
+                return redirect('/login')
+        return render(request, "shop/login.html")
+
+def logout_page(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request, "Logout Successfully")
+    return redirect("/")
+
+def register(request):
+    form = CustomUserForm()
+    if request.method == 'POST':
+        form = CustomUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Registration Successfully Completed")
+            return redirect('/login')
+    return render(request, "shop/register.html", {'form':form})
+
+def collections(request):    
+    category = Category.objects.filter(status=0)
+    return render(request, "shop/collections.html", {"category":category})
+
+def collectionsview(request, name):
+    if(Category.objects.filter(name=name, status=0)):
+        products = Product.objects.filter(category__name = name)
+        return render(request, "shop/products/index.html", {"products":products, "category_name":name})
+    else:
+        messages.warning(request, "No Such Category Found")
+        return redirect('collections')
+
+def product_details(request, cname, pname):
+    if(Category.objects.filter(name=cname, status=0)):
+        if(Product.objects.filter(name=pname, status=0)):
+            products = Product.objects.filter(name=pname, status=0).first()
+            return render(request, "shop/products/product_details.html", {"products":products})
+        else:
+            messages.warning(request, "No Such Category Found")
+            return redirect("collections")
+    else:
+        messages.warning(request, "No Such Category Found")
+        return redirect("collections")
+
+def add_to_cart(request):
+    if request.headers.get('X-Requested-With')=='XMLHttpRequest':
+        if request.user.is_authenticated:
+            data=json.load(request)
+            product_qty=data['product_qty']
+            product_id=data['pid']
+            product_status = Product.objects.get(id=product_id)
+            if product_status:
+                if Cart.objects.filter(user=request.user.id, product_id=product_id):
+                    return JsonResponse({'status':'Product already in Cart'}, status=200)
+                else:
+                    if product_status.quantity>=product_qty:
+                        Cart.objects.create(user=request.user, product_id=product_id, product_qty=product_qty)
+                        return JsonResponse({'status':'Cart Added'}, status=200)
+                    else:
+                        return JsonResponse({'status':'Product Not Available'}, status=200)
+        else:
+            return JsonResponse({'status':'Login to Add Cart'}, status=200)
+    else:
+        return JsonResponse({'status':'Invalid Access'}, status=200)
+
+def cart_page(request):
+    if request.user.is_authenticated:
+        cart = Cart.objects.filter(user=request.user)
+        return render(request, "shop/cart.html", {'cart':cart})
+    else:
+        return redirect('/')
+
+def remove_cart(request, id):
+    cartitem = Cart.objects.get(id=id)
+    cartitem.delete()
+    return redirect("/cart")
+
+def fav_page(request):
+    if request.headers.get('X-Requested-With')=='XMLHttpRequest':
+        if request.user.is_authenticated:
+            data=json.load(request)
+            product_id=data['pid']
+            product_status = Product.objects.get(id=product_id)
+            if product_status:
+                if Favourite.objects.filter(user=request.user.id, product_id=product_id):
+                    return JsonResponse({'status':'Product already in Favourite'}, status=200)
+                else:
+                    Favourite.objects.create(user=request.user, product_id=product_id)
+                    return JsonResponse({'status':'Product added to Favourite'}, status=200)
+        else:
+            return JsonResponse({'status':'Login to Add Favourite'}, status=200)
+    else:
+        return JsonResponse({'status':'Invalid Access'}, status=200)
+
+def favviewpage(request):
+    if request.user.is_authenticated:
+        fav = Favourite.objects.filter(user=request.user)
+        return render(request, "shop/fav.html", {'fav':fav})
+    else:
+        return redirect('/')
+
+def remove_fav(request, id):
+    favitem = Favourite.objects.get(id=id)
+    favitem.delete()
+    return redirect("/favviewpage")
